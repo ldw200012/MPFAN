e2fea469bc1570f8d39c9e7c4ab6df88fd90fc63
ldw200012_morin <dongwooklee1201@gmail.com>
2025-02-16 17:12:42 +0900

    Initial Commit

---
+++ configs_reid/_base_/datasets/reid_waymo_pts.py
@@ -0,0 +1,100 @@
+CLASSES = ['car',
+           'truck',
+           'bus',
+           'motorcycle',
+           'bicycle',
+           'pedestrian']
+
+tracking_classes = {
+    'bicycle':'bicycle',
+    'truck':'truck',
+    'car':'car',
+    'trailer':'trailer',
+    'bus':'bus',
+    'motorcycle':'motorcycle',
+    'pedestrian':'pedestrian'
+}
+
+tracking_classes_fp = tracking_classes
+
+cls_to_idx = {
+    'none_key':-1,
+    'car':0,
+    'truck':1,
+    'bus':2,
+    'motorcycle':3,
+    'bicycle':4,
+    'pedestrian':5
+}
+
+cls_to_idx_fp = {
+    'none_key':-1,
+    'car':0,
+    'truck':1,
+    'bus':2,
+    'motorcycle':3,
+    'bicycle':4,
+    'pedestrian':5,
+    'FP_car':6,
+    'FP_truck':7,
+    'FP_bus':8,
+    'FP_motorcycle':9,
+    'FP_bicycle':10,
+    'FP_pedestrian':11,
+}
+
+train_metadata_version = 'waymo-det-both-train'
+val_metadata_version = 'waymo-det-both-val'
+data = dict(
+    samples_per_gpu=64,
+    val_samples_per_gpu=128,
+    workers_per_gpu=4,
+    train=dict(type='ReIDDatasetWaymoFP',
+               train=True,
+               cls_to_idx=cls_to_idx,
+               cls_to_idx_fp=cls_to_idx_fp,
+               tracking_classes=tracking_classes,
+               tracking_classes_fp=tracking_classes_fp,
+               subsample_sparse=128,
+               subsample_mode="random",
+               val_subsample_mode="fps",
+               CLASSES=CLASSES,
+               return_mode='dict',
+               verbose=False,
+               validation_seed=0,
+               sparse_loader=dict(type='ObjectLoaderSparseWaymo',
+                                metadata_path='Datasets/Waymo-ReID/data/lstk/updated_sparse-{}/metadata'.format(train_metadata_version),
+                                data_root='Datasets/Waymo-ReID/data/lstk/updated_sparse-{}'.format(train_metadata_version),
+                                min_points=2,
+                                tracking_classes=tracking_classes,
+                                load_scene=True,
+                                load_objects=True,
+                                load_feats=['xyz'],
+                                load_dims=[3],),
+            ),
+    val=dict(type='ReIDDatasetWaymoFPValEven',
+               train=False,
+               cls_to_idx=cls_to_idx,
+               cls_to_idx_fp=cls_to_idx_fp,
+               tracking_classes=tracking_classes,
+               tracking_classes_fp=tracking_classes_fp,
+               subsample_sparse=128,
+               subsample_mode="random",
+               val_subsample_mode="fps",
+               CLASSES=CLASSES,
+               return_mode='dict',
+               verbose=False,
+               validation_seed=0,
+               max_combinations=10,
+               sparse_loader=dict(type='ObjectLoaderSparseWaymo',
+                                metadata_path='Datasets/Waymo-ReID/data/lstk/sparse-{}/metadata'.format(val_metadata_version),
+                                data_root='Datasets/Waymo-ReID/data/lstk/sparse-{}'.format(val_metadata_version),
+                                min_points=2,
+                                tracking_classes=tracking_classes,
+                                use_metdata_fix=True,
+                                load_scene=True,
+                                load_objects=True,
+                                load_feats=['xyz'],
+                                load_dims=[3],),
+            ),
+)
